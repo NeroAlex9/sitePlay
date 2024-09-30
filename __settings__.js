4b2c5b9616365a586b5f2045f75570c96121d83c
NeroAlex9 <Nerobelov1997@mail.ru>
2024-09-30 17:31:38 +0300

    start

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2078910.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 197276515 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/197320385/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/197320386/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/197320384/1/ammo.js', 'preload' : true},
+];
